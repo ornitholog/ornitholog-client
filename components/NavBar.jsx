01da4c5d98c63acad6a0ff93a7bd4cb8537d3ee3
Marco Salvoni <marco.salvoni8@gmail.com>
2023-12-07 14:44:47 +0100

    edit navbar components

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom"
+import "../src/styles/NavBar.css"
+
 function NavBar(){
     return(
         <>
-            <nav>This is a navbar</nav>
+            <nav className="NavBar">
+                <Link className="logo" to="/">OrnithoLog</Link>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="#">Profile</Link>
+                    </li>
+                    <li className="favourites-item">
+                        <Link to="#">Birds</Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
