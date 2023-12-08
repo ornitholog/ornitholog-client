a951b4ed91a2c567c2d9f121a9d92547344e8afb
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-08 11:43:44 +0100

    Setting up Log In Page and functuality

---
+++ context/auth.context.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = import.meta.env.VITE_API_URL;
+
+const AuthContext = React.createContext();
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [authError, setAuthError] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+    if (storedToken) {
+      axios
+        .get(`${url}/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          const user = response.data;
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(user);
+        })
+        .catch((error) => {
+          if (error) {
+            setAuthError(error.response.data.message);
+            return;
+          }
+          setIsLoggedIn(false);
+          setIsLoading(false);
+          setUser(null);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  };
+
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+  };
+
+  const logOutUser = () => {
+    removeToken();
+    authenticateUser();
+  };
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+        logOutUser,
+        authError,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthProviderWrapper, AuthContext };
--- pages/LogInPage.jsx
+++ pages/LogInPage.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function LogInPage() {
+  const url = import.meta.env.VITE_API_URL;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+
+    axios
+      .post(`${url}/auth/login`, requestBody)
+      .then((response) => {
+        console.log("JWT token", response.data.authToken);
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
   return (
     <>
-      <div className="container">
-        <h1>Sign in</h1>
+      <div className="LogInPage">
+        <div className="auth-container">
+          <h1>Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <label>E-mail:</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <input
+              type="file"
+              name="profileImg"
+              accept="image/png, image/jpeg"
+            />
+            <button className="submit-btn" type="submit">
+              Log In
+            </button>
+          </form>
+        </div>
+
+        {errorMessage && <p>{errorMessage}</p>}
       </div>
     </>
   );
