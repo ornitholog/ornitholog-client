8e5ca663119f10f1c51b17d9a47bad6b9a6f74b9
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-13 14:23:43 +0100

    fixing geo data issue

--- components/ObservationDetails.jsx
+++ components/ObservationDetails.jsx
@@ -5,11 +5,11 @@ import React, { useState, useEffect } from "react";
 
 function ObservationDetails({ observation, summary }) {
   const [locationDetails, setLocationDetails] = useState({});
+  const date = new Date(observation.date);
 
   const getGeoData = async (lat, long) => {
     try {
       const data = await service.geoCoder(lat, long);
-      console.log(data);
       setLocationDetails(data);
     } catch (error) {
       console.error("Error fetching geo data:", error);
@@ -18,7 +18,7 @@ function ObservationDetails({ observation, summary }) {
 
   useEffect(() => {
     getGeoData(
-      observation.location.coordinates[0],
+      observation.location.coordinates[1],
       observation.location.coordinates[0]
     );
   }, []);
@@ -26,13 +26,21 @@ function ObservationDetails({ observation, summary }) {
   return (
     <div key={observation._id} className="observation-card">
       <img src={observation.photo}></img>
+      <h4>{observation.title}</h4>
       <h4>{observation.birdId.name}</h4>
       <h5>{observation.birdId.sciName}</h5>
 
-      <div>{observation.date}</div>
-      <div>City: {locationDetails.city}</div>
-      <div>Country: {locationDetails.countryName}</div>
-      <div>Kontinent:{locationDetails.continent}</div>
+      <div>
+        {date.getDate()}-{date.getMonth() + 1}-{date.getFullYear()}
+      </div>
+      <div>
+        {observation.location.coordinates[0]} ,
+        {observation.location.coordinates[1]}
+      </div>
+      <div>
+        {locationDetails.city} - {locationDetails.countryName}
+      </div>
+      <div></div>
 
       <Link to={`/observations/${observation._id}`}>More Info</Link>
     </div>
