39a497538b63105788f9c44fde2e436746f6b715
mayakilbertus <maya.kilbertus@yahoo.de>
2023-12-14 09:54:13 +0100

    Edit Modal styling and exit btn

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,22 +6,20 @@ import { AuthContext } from "../context/auth.context";
 import IsPrivate from "../components/IsPrivate/IsPrivate";
 import IsAnon from "../components/IsAnon/IsAnon";
 import "../src/styles/NavBar.scss";
-import logo from "../src/assets/ornitholog-logo-white.svg"
-import logout from "../src/assets/logout.svg"
+import logo from "../src/assets/ornitholog-logo-white.svg";
+import logout from "../src/assets/logout.svg";
 
 function NavBar({ birdList, fetchObservationList }) {
-
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const [toggle, setToggle] = useState(false);
-  const [userId, setUserId] = useState(null)
+  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
-    if(user){
-      setUserId(user._id)
+    if (user) {
+      setUserId(user._id);
     }
-  }, [user])
+  }, [user]);
 
-  
   return (
     <>
       <nav className="NavBar">
@@ -32,10 +30,10 @@ function NavBar({ birdList, fetchObservationList }) {
           <li>
             <Link to="/">Home</Link>
           </li>
-          { isLoggedIn && userId && (
-          <li>
-            <Link to={`/profile/${userId}`}> Profile</Link>
-          </li>
+          {isLoggedIn && userId && (
+            <li>
+              <Link to={`/profile/${userId}`}> Profile</Link>
+            </li>
           )}
           <li>
             <Link to="/birds">Birds</Link>
@@ -48,14 +46,14 @@ function NavBar({ birdList, fetchObservationList }) {
           )}
           {isLoggedIn && (
             <>
-            <li>
-              <a onClick={() => logOutUser()} className="logout">
-                Logout
-              </a>
-            </li>
-            <button onClick={() => setToggle(!toggle)} className="btn">
-              Create observation
-            </button>
+              <li>
+                <a onClick={() => logOutUser()} className="logout">
+                  Logout
+                </a>
+              </li>
+              <button onClick={() => setToggle(!toggle)} className="btn">
+                Create observation
+              </button>
             </>
           )}
         </ul>
@@ -65,6 +63,7 @@ function NavBar({ birdList, fetchObservationList }) {
         <NewObservation
           birdList={birdList}
           fetchObservationList={fetchObservationList}
+          changeToggle={setToggle}
         />
       )}
     </>
